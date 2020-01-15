4f7cd1d7799136ea17977607e3c674765e18acaf
Jur van der Molen <jurvandermolen@gmail.com>
2020-01-15 19:25:10 +0100

    Joejoe

--- fys/wsgi/index.py
+++ fys/wsgi/index.py
@@ -1,25 +1,12 @@
 import urllib.parse as urlparse
+import logging
 
 def application(environ, start_response):
+    logging.error("HELP IK ZIT ONDER WATER!")
     status = '200 OK'
     response_header = [('Content-type', 'text/html')]
     start_response(status, response_header)
 
-    html = ''
-    html += '<html>\n'
-    html += '  <body>\n'
-    html += '    <title>Test WSGI page for fys</title>\n'
-    html += '    <div style="width: 100%; font-size: 20px; font-weight: bold; text-align: center;">\n'
-    html += '      Welcome to the mod_wsgi test page\n'
-    html += '    </div>\n'
-
-    html += 'You are logged in.\n'
-
-    html += '    <h2>Some interesting environment variables:</h2>\n'
-    html += '    <div style="width: 100%; font-family: Courier; font-size: 14px; font-weight: bold; text-align: left;">\n'
-
-    html += '   <h2>Request parameters</h2>\n'
-    html += '   <div style="width: 100%; font-family: Courier; font-size: 14px; font-weight: bold; text-align: left;">\n'
     # check HTTP request method and get parameters from requestmethod = environ.get('REQUEST_METHOD', '')
     method = environ.get('REQUEST_METHOD', '')
 
@@ -30,19 +17,16 @@ def application(environ, start_response):
         input = environ['wsgi.input'].read().decode()
         params = urlparse.parse_qs(input)
 
-    html += 'Userid: ' + params.get('userid', [''])[0] + '<br>\n'
-    html += 'Password: ' + params.get('password', [''])[0] + '<br>\n'
-
-    html += ' </div>\n'
-
-    environmentVars = ['REQUEST_METHOD', 'REQUEST_URI', 'QUERY_STRING', 'SCRIPT_NAME', 'HTTP_REFERER']
-    for envVar in environmentVars:
-        envVarValue = environ.get(envVar, '')
-        html += envVar + ' = ' + envVarValue + '<br>\n'
-
-    html += '    </div>\n'
-    html += '  </body>\n'
-    html += '</html>\n'
+    Userid = params.get('Gebruikersnaam', [''])[0]
+    Password = params.get('Wachtwoord', [''])[0]
+    
+# Maak dit relatief en niet absoluut
+    if Userid == 'Jur' and Password == '123':
+        fp = open('var/www/fys/calc.html', 'r')
+        html = fp.read()
+    else:
+        fp = open('var/www/fys/websitefout.html', 'r')
+        html = fp.read()
 
     return [bytes(html, 'utf-8')]
 
